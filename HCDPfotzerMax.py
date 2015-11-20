f7243e1ceeb1b5ee5171ce745a4bca965c0c753e
Andy Martinez <am41119@hotmail.com>
2015-11-20 01:02:40 +0100

    Basic test script for pfotzer maximum determination

--- HCDPfotzerMax.py
+++ HCDPfotzerMax.py
@@ -1,50 +1,71 @@
 from HCDDataReader import HCDDataReader
 from HCDDataProcessor import HCDDataProcessor
-import pylab
+import matplotlib.pyplot as plt
 import numpy as np
+from scipy import interpolate
 
 pressure_binsize = 0.5
 
-##############################################################################################
-########      BEXUS   ############
-print "BEXUS"
-BEXUS_reader = HCDDataReader("./Datasets/BEXUSDetectorFlightData")
-BEXUS_processor = HCDDataProcessor()
-BEXUS_data = BEXUS_processor.remove_peaks(BEXUS_reader.data)
+KMI_reader = HCDDataReader("./Datasets/KMI102014", software_version=1)
+KMI_processor = HCDDataProcessor()
+KMI_processor.remove_peaks(KMI_reader.data)
 
-pressure1_BX = BEXUS_data["press1"]
-pressure2_BX = BEXUS_data["press2"]
-pressure3_BX = BEXUS_data["press3"]
-loop_time_BX = BEXUS_data["looptimer"]
-HV_state_BX = BEXUS_data["hvstate"]
-eth_state_BX = BEXUS_data["ethstate"]
-
-gm1_BX_raw = np.array(BEXUS_data["gm1"])
-gm2_BX_raw = np.array(BEXUS_data["gm2"])
-gm3_BX_raw = np.array(BEXUS_data["gm3"])
-gm4_BX_raw = np.array(BEXUS_data["gm4"])
-
-gm1_BX = BEXUS_processor.datacut_based_on_statedata(gm1_BX_raw, HV_state_BX)
-gm2_BX = BEXUS_processor.datacut_based_on_statedata(gm2_BX_raw, HV_state_BX)
-gm3_BX = BEXUS_processor.datacut_based_on_statedata(gm3_BX_raw, HV_state_BX)
-gm4_BX = BEXUS_processor.datacut_based_on_statedata(gm4_BX_raw, HV_state_BX)
-
-mean_pressures_BX = []
-for i, time in enumerate(pressure1_BX):
-    if HV_state_BX[i] == 0:
-        mean_pressure = (pressure1_BX[i] + pressure2_BX[i] + pressure3_BX[i]) / 3
-        mean_pressures_BX.append(mean_pressure)
-
-gm1_BX_average, gm1_BX_error, gm1_BX_binpoints = BEXUS_processor.average_counts_over_pressure(gm1_BX, mean_pressures_BX, binsize=pressure_binsize)
-gm2_BX_average, gm2_BX_error, gm2_BX_binpoints = BEXUS_processor.average_counts_over_pressure(gm2_BX, mean_pressures_BX, binsize=pressure_binsize)
-gm3_BX_average, gm3_BX_error, gm3_BX_binpoints = BEXUS_processor.average_counts_over_pressure(gm3_BX, mean_pressures_BX, binsize=pressure_binsize)
-gm4_BX_average, gm4_BX_error, gm_4_BX_binpoints = BEXUS_processor.average_counts_over_pressure(gm4_BX, mean_pressures_BX, binsize=pressure_binsize)
+pressure_KMI = KMI_reader.data["press1"]
+gm1_KMI = np.array(KMI_reader.data["gm1"])
+gm2_KMI = np.array(KMI_reader.data["gm2"])
+gm3_KMI = np.array(KMI_reader.data["gm3"])
+gm4_KMI = np.array(KMI_reader.data["gm4"])
 
 
+gm1_KMI_average, gm1_KMI_error, gm1_KMI_binpoints = KMI_processor.average_counts_over_pressure(gm1_KMI, pressure_KMI, binsize=pressure_binsize)
+gm2_KMI_average, gm2_KMI_error, gm2_KMI_binpoints = KMI_processor.average_counts_over_pressure(gm2_KMI, pressure_KMI, binsize=pressure_binsize)
+gm3_KMI_average, gm3_KMI_error, gm3_KMI_binpoints = KMI_processor.average_counts_over_pressure(gm3_KMI, pressure_KMI, binsize=pressure_binsize)
+gm4_KMI_average, gm4_KMI_error, gm4_KMI_binpoints = KMI_processor.average_counts_over_pressure(gm4_KMI, pressure_KMI, binsize=pressure_binsize)
 
-pressures_BX_average, pressures_BX_error = BEXUS_processor.pressurebin_centers(mean_pressures_BX, binsize=pressure_binsize)
-gm_BX_weighted_average, gm_BX_weighted_error = BEXUS_processor.weighted_average([gm1_BX_average, gm2_BX_average, gm3_BX_average, gm4_BX_average], [gm1_BX_error, gm2_BX_error, gm3_BX_error, gm4_BX_error])
+
+pressures_KMI_average, pressures_KMI_error = KMI_processor.pressurebin_centers(pressure_KMI, binsize=pressure_binsize)
+gm_KMI_weighted_average, gm_KMI_weighted_error = KMI_processor.weighted_average([gm1_KMI_average, gm2_KMI_average, gm3_KMI_average, gm4_KMI_average], [gm1_KMI_error, gm2_KMI_error, gm3_KMI_error, gm4_KMI_error])
 
 #####################################################################################################################
+peak_boundaries = KMI_processor.set_peak_boundaries(xmin=1.853304, xmax=12.565059)
+#peak_boundaries = KMI_processor.set_peak_boundaries(x=pressures_KMI_average, y=gm_KMI_weighted_average)
+pressure_peak, gm_counts_peak = KMI_processor.extract_peak([pressures_KMI_average, pressures_KMI_error], [gm_KMI_weighted_average, gm_KMI_weighted_error])
+weight = []
+for error in gm_counts_peak[1]:
+    weight.append(1/error)
 
+sorted_x, sorted_y, sorted_weight = (list(x) for x in zip(*sorted(zip(pressure_peak[0], gm_counts_peak[0], weight))))
+
+basic_spline = interpolate.UnivariateSpline(sorted_x, sorted_y, w=sorted_weight, s=1, k=4)
+derivative_spline = basic_spline.derivative(1)
+
+xnew = np.arange(min(pressure_peak[0]), max(pressure_peak[0]), 0.01)
+ynew_der = derivative_spline(xnew)
+ynew = basic_spline(xnew)
+
+roots = derivative_spline.roots()
+print roots
+coefs = derivative_spline.get_coeffs()
+print coefs
+knots = derivative_spline.get_knots()
+print knots
+
+average_count = np.average(gm_counts_peak[0])
+SStot = 0
+for count in gm_counts_peak[0]:
+    SStot += (count - average_count) ** 2
+SSres = basic_spline.get_residual()
+Rsquared = 1 - SSres / SStot
+print Rsquared
+
+
+
+
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+ax.errorbar(pressure_peak[0], gm_counts_peak[0], yerr=gm_counts_peak[1], fmt="o")
+#plt.plot(xnew, ynew_der)
+plt.plot(xnew, ynew)
+plt.title('Quadratic-spline interpolation')
+plt.show()
 
